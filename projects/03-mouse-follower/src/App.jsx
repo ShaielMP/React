2f6ebdc8f5af6e85a9433a50a7276307b360027a
ShaielMP <shaielmp0@gmail.com>
2025-09-11 18:12:46 +0200

    adding pointer

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,9 +1,33 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
+  const [enabled, setEnabled] = useState(false)
+
+  useEffect(() => {
+    console.log('effect', { enabled })
+  }, [enabled])
   return (
-    <h3>Proyecto 3</h3>
+    <main>
+    <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
+        transform: `translate(0px, 0px)`
+      }}
+      />
+      <h3>Proyecto 3</h3>
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+      </button>
+    </main>
   )
 }
 
