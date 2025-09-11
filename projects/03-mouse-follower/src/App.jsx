fac45fce46da5862c300bb9d5c729ee0b89b5593
ShaielMP <shaielmp0@gmail.com>
2025-09-11 19:43:50 +0200

    finished mouse follower project

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,14 +1,38 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 
-function App() {
+const FollowMouse = () => {
+  // Estados de la app
   const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
 
+  // Efecto del mouse
   useEffect(() => {
     console.log('effect', { enabled })
+
+    // Limpiar el listener cuando el componente desaparece
+    const handleMove = (event) => {
+      // Actualizar la posición
+      const { clientX, clientY } = event
+      console.log('handleMove', { clientX, clientY })
+      // Actualizar el estado
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enabled) {
+      // Agregar el listener cuando enabled es true
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    return () => {
+      // Limpiar el listener cuando enabled es false
+      console.log('cleanup')
+      window.removeEventListener('pointermove', handleMove)
+    }
   }, [enabled])
+
   return (
-    <main>
+    <>
     <div style={{
         position: 'absolute',
         backgroundColor: 'rgba(0, 0, 0, 0.5)',
@@ -20,13 +44,21 @@ function App() {
         top: -25,
         width: 50,
         height: 50,
-        transform: `translate(0px, 0px)`
+        transform: `translate(${position.x}px, ${position.y}px)`
       }}
       />
       <h3>Proyecto 3</h3>
       <button onClick={() => setEnabled(!enabled)}>
         {enabled ? 'Desactivar' : 'Activar'} seguir puntero
       </button>
+    </>
+  )
+}
+
+function App() {
+  return (
+    <main>
+      <FollowMouse />
     </main>
   )
 }
